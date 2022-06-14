c3cf6ce44f5a571e5aa12a93507d812176b36ebb
iosmncteams <dwijayanto.taufik@mncgroup.com>
2022-06-14 15:02:20 +0700

    add code verifier & challenge

--- Sources/SsoRedirect/SsoSign.swift
+++ Sources/SsoRedirect/SsoSign.swift
@@ -16,6 +16,9 @@ public class SsoSign: UIViewController {
     
     var contextProvider: AuthContextProvider?
     
+    var codeVerifier: String?
+    var codeChallenge: String?
+    
     override public func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -30,13 +33,19 @@ public class SsoSign: UIViewController {
         return keyWindow?.rootViewController
     }
     
+    public func initialize() {
+        codeVerifier = PKCE.generateCodeVerifier()
+        codeChallenge = PKCE.generateCodeChallenge(from: codeVerifier!)
+        
+        print("CODE VERiFIer: \(codeVerifier!)")
+    }
+    
     public func login() {
-//        let vc = Browser()
-//        getTopMostViewController()?.present(vc, animated: true)
+        print("CODE Challenge: \(codeChallenge!)")
         //Initialize auth session
         let callbackUrl = "sampleproj://sample.com?"
         let callbackURI = callbackUrl.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
-        self.authSession = ASWebAuthenticationSession(url: URL(string: "https://dev-passport.rctiplus.com/login?application_id=5669acac-d4a1-4f5e-87ca-0d989df5efa7&redirect_uri=sampleproj://sample.com&scope=openid%20profile%20email&code_challenge=gptOwceoBveAK7QbKBcQxb59_aiaLkdtQHabaElaVGo&code_challenge_method=S256&response_type=code&state=1234567890")!, callbackURLScheme: callbackURI, completionHandler: { (callBack:URL?, error:Error? ) in
+        self.authSession = ASWebAuthenticationSession(url: URL(string: "https://dev-passport.rctiplus.com/login?application_id=5669acac-d4a1-4f5e-87ca-0d989df5efa7&redirect_uri=sampleproj://sample.com&scope=openid%20profile%20email&code_challenge=\(codeChallenge!)&code_challenge_method=S256&response_type=code&state=1234567890")!, callbackURLScheme: callbackURI, completionHandler: { (callBack:URL?, error:Error? ) in
             guard error == nil, let successURL = callBack else {
                 print(error!)
                 return
